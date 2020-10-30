9fab6a805cb4ba649f3fe11660fcf6899b0a5127
BenKMorley <bkm1n18@soton.ac.uk>
2020-10-30 13:50:34 +0000

    Added more output information

--- frequentist_run.py
+++ frequentist_run.py
@@ -31,7 +31,7 @@ from scipy.linalg import logm
 from tqdm import tqdm
 
 
-def run_frequentist_analysis(input_h5_file, model, N_s, g_s, L_s, Bbar_s,
+def run_frequentist_analysis(input_h5_file, model, N_s, g_s, L_s, Bbar_s_in,
                              GL_min, GL_max, param_names, x0, method="BFGS",
                              no_samples=500, run_bootstrap=True,
                              print_info=True):
@@ -49,8 +49,8 @@ def run_frequentist_analysis(input_h5_file, model, N_s, g_s, L_s, Bbar_s,
             studied, ag values should be floats
         L_s: List of L / a (Lattice Size) values to be studied, L values should
             be ints
-        Bbar_s: List of Bbar crossing values to be studied, Bbar values should
-            be floats
+        Bbar_s_in: List of Bbar crossing values to be studied, Bbar values
+            should be floats
         GL_min: Float, minimum value of the product (ag) * (L / a) to be
             included in the fit
         GL_max: Float, maximum value of the product (ag) * (L / a) to be
@@ -84,13 +84,14 @@ def run_frequentist_analysis(input_h5_file, model, N_s, g_s, L_s, Bbar_s,
     """
 
     samples, g_s, L_s, Bbar_s, m_s = load_h5_data(input_h5_file, N_s, g_s, L_s,
-                                                  Bbar_s, GL_min, GL_max)
+                                                  Bbar_s_in, GL_min, GL_max)
+    N = N_s[0]
 
     cov_matrix, different_ensemble = cov_matrix_calc(g_s, L_s, m_s, samples)
     cov_1_2 = numpy.linalg.cholesky(cov_matrix)
     cov_inv = numpy.linalg.inv(cov_1_2)
 
-    res_function = make_res_function(N_s[0], m_s, g_s, L_s, Bbar_s)
+    res_function = make_res_function(N, m_s, g_s, L_s, Bbar_s)
 
     # Using scipy.
     if method == "least_squares":
@@ -109,6 +110,9 @@ def run_frequentist_analysis(input_h5_file, model, N_s, g_s, L_s, Bbar_s,
     param_central = res.x
 
     if print_info:
+        print("###########################################################")
+        print(f"Config: N = {N}, Bbar_s = [{Bbar_s_in[0]}, {Bbar_s_in[1]}],"
+              f" gL_min = {GL_min}, gL_max = {GL_max}")
         print(f"chisq = {chisq}")
         print(f"chisq/dof = {chisq / dof}")
         print(f"pvalue = {p}")
