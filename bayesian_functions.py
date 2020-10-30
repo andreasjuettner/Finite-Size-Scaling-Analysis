4eab76713558715f6b5894fa909d8da2721917bb
BenKMorley <bkm1n18@soton.ac.uk>
2020-10-30 12:54:49 +0000

    Added licence

--- bayesian_functions.py
+++ bayesian_functions.py
@@ -1,3 +1,31 @@
+###############################################################################
+# Copyright (C) 2020
+#
+# Author: Ben Kitching-Morley bkm1n18@soton.ac.uk
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# See the full license in the file "LICENSE" in the top level distribution
+# directory
+
+# The code has been used for the analysis presented in
+# "Nonperturbative infrared finiteness in super-renormalisable scalar quantum
+# field theory" https://arxiv.org/abs/2009.14768
+###############################################################################
+
+
 from model_definitions import *
 import pymultinest
 import os
