944f1bb175659f3092cfe23de7a69452bd22ca9b
BenKMorley <bkm1n18@soton.ac.uk>
2021-01-20 16:27:40 +0000

    Added conditional so that the code will only look for mlims for a given g, L if they are defined

--- Binderanalysis.py
+++ Binderanalysis.py
@@ -586,9 +586,11 @@ def compute_Bindercrossing(N, g, Bbar, Lin):
 
     # tailored min and max to extrapolate beyond simulatedpoints by means of
     # reweighting
-    if mlims[sn][iLin]:
-        xmin = -mlims[sn][iLin][1]
-        xmax = -mlims[sn][iLin][0]
+    if sn in mlims:
+        if iLin in mlims[sn]:
+            if mlims[sn][iLin]:
+                xmin = -mlims[sn][iLin][1]
+                xmax = -mlims[sn][iLin][0]
 
     # compute integrated autocorrelation time for basic quantities for each
     # ensemble and bin data correspondingly
